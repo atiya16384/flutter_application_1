ac2cbc3e206ef91f936fa64fff4a24751561dcc5
atiya16384 <atiyahussain17170@outlook.com>
2023-07-13 21:33:17 +0100

    editing symptom_tracker to add output to screen

--- lib/pages/pages_navigate/symptom_tracker.dart
+++ lib/pages/pages_navigate/symptom_tracker.dart
@@ -1,10 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../features/symptom_tracker/add_symptom.dart';
 
 // the widget associated with the symptom tracker option
-
 class trackSymp extends StatefulWidget {
   // we add a route
   static route() => MaterialPageRoute(
@@ -15,6 +15,12 @@ class trackSymp extends StatefulWidget {
   State<trackSymp> createState() => _trackSympState();
 }
 
+// @override
+// void initState() {
+//   getlistIDs();
+//   super.initState();
+// }
+
 class _trackSympState extends State<trackSymp> {
   void signOut() {
     FirebaseAuth.instance.signOut();
@@ -24,11 +30,25 @@ class _trackSympState extends State<trackSymp> {
     Navigator.push(context, CreateSymptomTab.route());
   }
 
+  // Stream<List<User>> readUsers() => FirebaseFirestore.instance
+  //     .collection('users')
+  //     .snapshots()
+  //     .map((snapshot) =>
+  //         //what is doc
+  //         //we get the data from the json file and convert to list
+  //         snapshot.docs.map(((doc) => User.fromJson(doc.data()))).toList());
+
+  // Widget buildUser(User user) => ListTile(
+  //       subtitle: Text(user.symptom + user.prescription),
+  //       //title: Text(user.prescription),
+  //     );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromRGBO(228, 92, 162, 100),
       appBar: AppBar(
+        title: Text('Add data below'),
         backgroundColor: Colors.black,
         actions: [
           IconButton(
@@ -44,7 +64,6 @@ class _trackSympState extends State<trackSymp> {
         child: const Icon(
           Icons.add,
           color: Colors.white,
-
           //Change background of button to pink
           //change
         ),

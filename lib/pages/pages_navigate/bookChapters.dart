92c163034e497438b16cb0dcf85114c072acfe51
atiya16384 <atiyahussain17170@outlook.com>
2023-12-28 02:56:19 +0000

    build: not building, need to fix error

--- lib/pages/pages_navigate/bookChapters.dart
+++ lib/pages/pages_navigate/bookChapters.dart
@@ -1,47 +1,99 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dropdown_search/flutter_dropdown_search.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class bookChapters extends StatefulWidget {
+class BookChapters extends StatefulWidget {
   @override
-  State<bookChapters> createState() => _bookChaptersState();
+  State<BookChapters> createState() => _BookChaptersState();
 }
 
-class _bookChaptersState extends State<bookChapters> {
-  final controller = PageController();
-
-  @override
-  void dispose() {
-    controller.dispose();
-
-    super.dispose();
-  }
+class _BookChaptersState extends State<BookChapters> {
+  String? selectedChapterId;
+  Map<String, dynamic>? selectedChapter;
 
   void signOut() {
     FirebaseAuth.instance.signOut();
   }
 
+  Stream<List<Map<String, dynamic>>> readChapters() {
+    return FirebaseFirestore.instance
+        .collection('chapters')
+        .snapshots()
+        .map((snapshot) => snapshot.docs.map((doc) {
+              var data = doc.data() as Map<String, dynamic>;
+              data['id'] = doc.id; // Capture the document ID
+              return data;
+            }).toList());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color.fromRGBO(228, 92, 162, 100),
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          actions: [
-            IconButton(
-              onPressed: signOut,
-              icon: const Icon(Icons.logout),
-            )
-          ],
-        ),
-        body: PageView(children: [
-          //  controller: pageController,
+      backgroundColor: Color.fromRGBO(228, 92, 162, 100),
+      appBar: AppBar(
+        title: const Text('Book Chapters'),
+        backgroundColor: Colors.black,
+        actions: [
+          IconButton(
+            onPressed: signOut,
+            icon: const Icon(Icons.logout),
+          )
+        ],
+      ),
+      body: StreamBuilder<List<Map<String, dynamic>>>(
+        stream: readChapters(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No chapters available.'));
+          }
 
-          Container(
-              color: Colors.red, child: const Center(child: Text('Page 1'))),
-          Container(
-              color: Colors.indigo, child: const Center(child: Text('Page 2'))),
-          Container(
-              color: Colors.green, child: const Center(child: Text('Page 3'))),
-        ]));
+          var chapters = snapshot.data!;
+
+          return Column(
+            children: [
+              DropdownSearch<Map<String, dynamic>>(
+                items: chapters,
+                dropdownSearchDecoration: const InputDecoration(
+                  labelText: "Select Chapter",
+                  contentPadding: EdgeInsets.fromLTRB(12, 12, 0, 0),
+                  border: OutlineInputBorder(),
+                ),
+                popupItemBuilder: (context, item, isSelected) {
+                  return Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text(item['title'] ?? 'No Title'),
+                  );
+                },
+                onChanged: (value) {
+                  if (value != null) {
+                    setState(() {
+                      selectedChapterId = value['id'];
+                      selectedChapter = value;
+                    });
+                  }
+                },
+                selectedItem: selectedChapter,
+                showSearchBox: true,
+                itemAsString: (item) => item['title'] ?? 'No Title',
+              ),
+              Expanded(
+                child: SingleChildScrollView(
+                  padding: const EdgeInsets.all(16),
+                  child: selectedChapter != null
+                      ? Text(
+                          selectedChapter!['content'] ?? 'No Content',
+                          style: const TextStyle(fontSize: 18),
+                        )
+                      : const Center(child: Text('Please select a chapter')),
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
   }
 }
